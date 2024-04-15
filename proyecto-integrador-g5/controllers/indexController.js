ba3deece8e3d8d19759e98868e6f88b7ea6779d5
julietagardey <jgardey@udesa.edu.ar>
2024-04-15 16:10:30 -0300

    corrección de links y lógica de vistas

--- proyecto-integrador-g5/controllers/indexController.js
+++ proyecto-integrador-g5/controllers/indexController.js
@@ -2,7 +2,7 @@ const datos = require("../db/index");
 
 const indexController = {
     index: function (req, res) {
-        return res.render("index")
+        return res.render("index", {productos: datos.productos})
     },
     search: function (req, res) {
         return res.render("search-results")
