0722eeb891bb1ffa7753ae393a35949000ad29c9
moranoguer <nmora@udesa.edu.ar>
2024-06-03 10:58:37 -0300

    express-validator, hice las validaciones. Me falta el mensaje de error

--- proyecto-integrador-g5/routes/products.js
+++ proyecto-integrador-g5/routes/products.js
@@ -1,6 +1,19 @@
 var express = require('express');
 var router = express.Router();
 const productController = require("../controllers/productController")
+const{body} = require('express-validator');
+const controller = require('../controllers/productController')
+
+//Validaciones
+// TENEMOS QUE CAMBIAR EL FORMULARIO DE LAS VISTAS POR --> productform y asi...
+const validateProductform = [
+    body('email').isEmail().withMessage('Debes completar un email válido'),
+    body('usuario').notEmpty().withMessage('Deber completar el campo Usuario'),
+    body('fechaDeNacimiento').isDate().withMessage('Deber completar el campo Fecha de Nacimiento'),
+    body('numeroDeDocumento').isNumeric().withMessage('Deber completar el campo Numero de Documento'),
+    body('fotoPerfil').notEmpty().withMessage('Deber completar el campo Foto Perfil'),
+]
+
 
 router.get("/detail/:id", productController.detail)
 router.get("/add", productController.add)
--- proyecto-integrador-g5/routes/users.js
+++ proyecto-integrador-g5/routes/users.js
@@ -1,6 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var userController = require("../controllers/userController")
+const{body} = require('express-validator');
+const controller = require('../controllers/userController')
+
+//Validaciones
+// TENEMOS QUE CAMBIAR EL FORMULARIO DE LAS VISTAS POR --> Loginform y asi...
+const validateLoginform = [
+    body('email').isEmail().withMessage('Debes completar un email válido')
+]
+
 
 /* GET users listing. */
 router.get('/login', userController.login);
